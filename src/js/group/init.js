f74fd48cb0bee9288d836f8370469a22e3b5eb3c
강혜준 <vxggangxv@naver.com>
2017-12-21 17:34:49 +0900

    룸 팝업 추가

--- src/js/group/init.js
+++ src/js/group/init.js
@@ -1,15 +1,57 @@
-$(function() {
-	$(".popOpen").on('click', function() {
-		//console.log('hi')
-		
+$(function () {
+	// 단체문의 팝업
+	$(".popOpen").on('click', function () {
+
 		var thisObj = $(this);
 		//var popTop = thisObj.offset().top;
 		var popTop = $(window).scrollTop() + 100;
-		
-		
-		$('#contact-pop').css("top",popTop);
-		$('#contact-pop, #contact-bg').show();
-		
-	});
-});
 
+
+		$('#contact-pop').css("top", popTop);
+		$('#contact-pop, #contact-bg').show();
+
+	});
+
+	// 객실안내 자세히보기 클릭
+	$('#roomList .d-row-1 .a-btn').on('click', function () {
+		/*var roomTop = $(this).closest('li').offset().top - 300;
+		$(window).scrollTop(roomTop);*/
+		
+		$('#roomList > li .bxBox').css({
+			visibility: 'hidden',
+			height: 0
+		});
+		
+
+		if ($(this).closest('li').find('.bxBox').css('visibility') == 'hidden') {
+			
+			$(this).closest('li').find('.bxBox').css({
+				visibility: 'visible',
+				height: 'auto'
+			});
+			$(this).closest('li').find('.roomSlider').bxSlider({
+				auto: true,
+				autoControls: true,
+				speed: 500,
+				duration: 3000,
+				prevText: '<img src="http://img.ocean2you.co.kr/btn_prev2.png" alt="이전">',
+				nextText: '<img src="http://img.ocean2you.co.kr/btn_next2.png" alt="다음">',
+			});
+		} else {
+			
+		}
+	});
+	
+	// 팝업 닫기 클릭시 z-index 복귀
+	$('#roomList > li .bxBox .a-x').on('click', function () {
+		$(this).closest('li').find('.bxBox').css({
+			visibility: 'hidden',
+			height: 0
+		});
+		$(this).closest('li').find('.roomSlider').bxSlider({
+			autoControls: false
+		});
+	});
+	
+	
+});
